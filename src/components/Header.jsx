1a844ca334c29191ad0a66aaa06834337d31b4ce
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2022-04-08 10:59:30 -0300

    feat: ensure Header renders buttons according to the received props

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// eslint-disable-next-line no-unused-vars
 const Header = ({ props: { title, buttons } }) => (
   <header className="flex flex-row justify-between mx-3 my-8 max-w-screen-xl m-auto">
     <h1 className="text-6xl font-bold text-neutral-800">
       { title }
     </h1>
+    <div>
+      { buttons && buttons.map((button) => (
+        <button
+          key={button.label}
+          className="px-7 hover:bg-amber-400 text-xl font-extralight rounded-3xl bg-amber-500 text-zinc-50"
+          type="button"
+          onClick={button.onClick}
+          data-testid="HeaderButton"
+        >
+          {button.label}
+        </button>
+      ))}
+    </div>
   </header>
 );
 
