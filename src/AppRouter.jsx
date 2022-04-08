6503db7872ad56fa18ad1fdf795aa50f5aafe5d5
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2022-04-08 17:08:26 -0300

    feat: ensure EditCourse render

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AddCourse from './pages/AddCourse/AddCourse';
+import EditCourse from './pages/EditCourse/EditCourse';
 
 const AppRouter = () => (
   <Router>
@@ -9,6 +10,7 @@ const AppRouter = () => (
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/add" element={<AddCourse />} />
+        <Route path="/course/:id" element={<EditCourse />} />
       </Routes>
     </Suspense>
   </Router>
---
+++ src/pages/EditCourse/EditCourse.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EditCourse = () => (
+  <div data-testid="edit-course-component">EditCourse</div>
+);
+
+export default EditCourse;
