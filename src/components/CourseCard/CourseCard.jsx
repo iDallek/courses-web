2d07c42b92949350fea0f93d7842f9a69c0ac41d
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2022-04-08 11:45:08 -0300

    feat: ensure CourseCard renders title according to the received props

--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CourseCard = ({ content: { id } }) => (
+const CourseCard = ({ content: { id, title } }) => (
   <div role="article">
     <span role="heading" aria-level={2}>
       { `${id}` }
     </span>
+    <h1>
+      { title }
+    </h1>
   </div>
 );
 
@@ -14,5 +17,6 @@ export default CourseCard;
 CourseCard.propTypes = {
   content: PropTypes.shape({
     id: PropTypes.number,
+    title: PropTypes.string,
   }).isRequired,
 };
