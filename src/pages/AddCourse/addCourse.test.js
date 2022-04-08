09fc7fe05d65d9bd222190ecf8f92144e38ecfb5
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2022-04-08 16:54:09 -0300

    fix: add MemoryRouter to test

--- src/pages/AddCourse/addCourse.test.js
+++ src/pages/AddCourse/addCourse.test.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import AddCourse from './AddCourse';
 
 describe('<AddCourse />', () => {
   test('it should render the component', () => {
-    render(<AddCourse />);
+    render(<MemoryRouter><AddCourse /></MemoryRouter>);
     expect(screen.getByTestId('add-course-component')).toBeInTheDocument();
   });
 
   test('ensure AddCourse submit button is enabled only if all form fields are filled', () => {
-    render(<AddCourse />);
+    render(<MemoryRouter><AddCourse /></MemoryRouter>);
 
     const submitButton = screen.getByRole('button', { name: 'Salvar' });
     expect(submitButton).toBeDisabled();
