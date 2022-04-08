26bc191998747e234a70b7f779521da2a5a298e2
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2022-04-08 09:46:18 -0300

    test: ensure Header renders title according to the received props

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Header from './Header';
 
 describe('<Header />', () => {
@@ -16,5 +16,20 @@ describe('<Header />', () => {
     const header = container.querySelector('header');
 
     expect(header).toBeInTheDocument();
+  });
+
+  test('ensure Header renders title according to the received props', () => {
+    const fakeProps = {
+      title: 'any title',
+      buttons: [
+        { label: 'button 1', onClick: () => {} },
+        { label: 'button 2', onClick: () => {} },
+      ],
+    };
+
+    render(<Header props={fakeProps} />);
+    const title = screen.getByRole('heading');
+
+    expect(title).toHaveTextContent(fakeProps.title);
   });
 });
