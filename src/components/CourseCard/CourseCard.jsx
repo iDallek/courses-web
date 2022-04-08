770106ad878955188fef59d83b5d12f3f76e9c35
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2022-04-08 12:07:52 -0300

    feat: ensure CourseCard call onClick function passed by props

--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CourseCard = ({ content: { id, title, description } }) => (
-  <div role="article">
-    <span role="heading" aria-level={2}>
-      { `${id}` }
-    </span>
-    <h1>
-      { title }
-    </h1>
-    <p role="paragraph">
-      { description }
-    </p>
+const CourseCard = ({
+  content: {
+    id, title, description, onClick,
+  },
+}) => (
+  // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+  <div onClick={onClick} role="button" tabIndex="0">
+    <div role="article">
+      <span role="heading" aria-level={2}>
+        { `${id}` }
+      </span>
+      <h1>
+        { title }
+      </h1>
+      <p role="paragraph">
+        { description }
+      </p>
+    </div>
   </div>
 );
 
@@ -22,5 +29,6 @@ CourseCard.propTypes = {
     id: PropTypes.number,
     title: PropTypes.string,
     description: PropTypes.string,
+    onClick: PropTypes.func,
   }).isRequired,
 };
