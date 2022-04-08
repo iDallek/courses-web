1822abe1de767fefa90dc2e0d4298d1fc1fd8f4f
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2022-04-08 09:46:02 -0300

    feat: ensure Header renders title according to the received props

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // eslint-disable-next-line no-unused-vars
-const Header = ({ title, buttons }) => (
-  <header>
-    Header
+const Header = ({ props: { title, buttons } }) => (
+  <header className="flex flex-row justify-between mx-3 my-8 max-w-screen-xl m-auto">
+    <h1 className="text-6xl font-bold text-neutral-800">
+      { title }
+    </h1>
   </header>
 );
 
 export default Header;
 
 Header.propTypes = {
-  title: PropTypes.string.isRequired,
-  buttons: PropTypes.arrayOf(PropTypes.shape({
-    label: PropTypes.string,
-    onClick: PropTypes.func,
-  })).isRequired,
+  props: PropTypes.shape({
+    title: PropTypes.string,
+    buttons: PropTypes.arrayOf(PropTypes.shape({
+      label: PropTypes.string,
+      onClick: PropTypes.func,
+    })),
+  }).isRequired,
 };
