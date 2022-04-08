e9601f619d9fb13e3fe303ceecc0e7ce6d26181f
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2022-04-08 15:46:08 -0300

    feat: ensure AddCourse render

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,12 +1,14 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import AddCourse from './pages/AddCourse/AddCourse';
 
 const AppRouter = () => (
   <Router>
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/add" element={<AddCourse />} />
       </Routes>
     </Suspense>
   </Router>
---
+++ src/pages/AddCourse/AddCourse.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AddCourse = () => (
+  <div data-testid="add-course-component">
+    AddCourse
+  </div>
+);
+
+export default AddCourse;
