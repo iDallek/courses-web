aa1963d13593fbf296af2e065277d43182e13871
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2022-04-08 11:00:16 -0300

    test: ensure Header renders buttons according to the received props

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Header from './Header';
 
 describe('<Header />', () => {
@@ -31,5 +31,28 @@ describe('<Header />', () => {
     const title = screen.getByRole('heading');
 
     expect(title).toHaveTextContent(fakeProps.title);
+  });
+
+  test('ensure Header renders buttons according to the received props', () => {
+    const fakeProps = {
+      title: 'any title',
+      buttons: [
+        { label: 'button 1', onClick: jest.fn() },
+        { label: 'button 2', onClick: jest.fn() },
+      ],
+    };
+
+    render(<Header props={fakeProps} />);
+    const buttons = screen.getAllByRole('button');
+
+    expect(buttons).toHaveLength(fakeProps.buttons.length);
+
+    expect(buttons[0]).toHaveTextContent(fakeProps.buttons[0].label);
+    expect(buttons[1]).toHaveTextContent(fakeProps.buttons[1].label);
+
+    fakeProps.buttons.forEach((button, index) => {
+      fireEvent.click(buttons[index]);
+      expect(button.onClick).toHaveBeenCalledTimes(1);
+    });
   });
 });
